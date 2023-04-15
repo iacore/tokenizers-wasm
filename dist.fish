25755b45a1e32c519b2944e42c790c0cb2cf00a9
Locria Cyber <74560659+iacore@users.noreply.github.com>
2023-04-15 04:04:15 +0000

    Add release

--- dist.fish
+++ dist.fish
@@ -4,3 +4,9 @@ wasm-pack build
 and pnpm i
 and pnpm build
 and cp pkg/tokenizers_wasm.d.ts output
+
+and begin
+    pushd output
+    tar --owner=0 --group=0 --touch -cvzf ../release.tar.gz .
+    popd
+end
